77dcf163998ac6635be400118b44762a942e2887
shelalaine <schan211@my.centennialcollege.ca>
2017-01-10 22:06:41 -0500

    Add code to display the button clicked in the label.

--- MAPD124-W2017-Lesson1/ViewController.swift
+++ MAPD124-W2017-Lesson1/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var appLabel: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        
+        appLabel.text = "\(sender.currentTitle!) touched"
     }
-
 
 }
 
