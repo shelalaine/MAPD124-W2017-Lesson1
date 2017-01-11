e764ebb62f2867d2af32000e89e9cbf26f59e575
shelalaine <schan211@my.centennialcollege.ca>
2017-01-10 21:22:33 -0500

    Initial Commit

---
+++ MAPD124-W2017-Lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD124-W2017-Lesson1
+//
+//  Created by Shelalaine Chan on 2017-01-10.
+//  Copyright © 2017 shelalainechan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
